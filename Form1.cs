e4744b61071063e5bb371b4f8827c14c9f84c2c6
VictorTF15 <utp0210623@alumno.utpuebla.edu.mx>
2025-05-20 22:34:47 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoggerUnico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Logger log1 = Logger.GetInstancia();
+            Logger log2 = Logger.GetInstancia();
+
+            log1.Log("chamoy@");
+            log2.Log("Mensaje 2");
+
+
+            bool sonIguales = log1 == log2;
+            string mensaje = sonIguales ? "Son iguales" : "No son iguales";
+            MessageBox.Show(mensaje);
+
+        }
+
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerUnico
+{
+    class Logger
+    {
+        private static Logger instancia;
+        private Logger() { }
+        public static Logger GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Logger();
+            }
+            return instancia;
+        }
+        public string Log(string mensaje)
+        {
+            return $"[LOG]: {mensaje}";
+        }
+    }
+}
